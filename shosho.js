d36af0411d1a496dd6929f97f6882cd47dd70d54
weedsman530 <87883725+weedsman530@users.noreply.github.com>
2025-09-13 20:36:53 +0300

    Delete shosho.js

--- shosho.js
+++
@@ -1,6 +0,0 @@
-const list = document.querySelectorAll('.list')
-function active()  {
-    list.forEach((item)=> item.classList.remove('active'));
-    this.classList.add('active'); 
-}
-list.forEach((item)=> item.addEventListener('click' , active));
